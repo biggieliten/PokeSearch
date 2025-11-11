b4b02a05875cb8a5a7c260b14fa9d327a9d3c730
biggieliten <filipisaksson00@gmail.com>
2025-11-11 09:40:47 +0100

    create reverse proxy for pokeapi

--- server/PokéReverseProxy/Program.cs
+++ server/PokéReverseProxy/Program.cs
@@ -1,7 +1,54 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddCors();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapReverseProxy();
+HttpClient httpClient = new HttpClient();
+
+app.UseCors(policy => policy.AllowAnyOrigin());
+
+async Task<IResult> GetPokemon(string identifier)
+{
+	try
+	{
+		var response = await httpClient.GetAsync($"https://pokeapi.co/api/v2/pokemon/{identifier}");
+
+		if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+		{
+			return Results.NotFound(new { error = $"Pokémon '{identifier}' not found" });
+		}
+
+		var content = await response.Content.ReadFromJsonAsync<PokemonResponse>();
+		return Results.Ok(new
+		{
+			id = content.Id,
+			name = content.Name ?? "",
+			image = content.Sprites.FrontDefault
+		});
+	}
+	catch
+	{
+		return Results.StatusCode(500);
+	}
+}
+
+app.MapGet("/api/pokemon/{name}", async (string name) => await GetPokemon(name));
+
+app.MapGet("/api/pokemon/{n:int}", async (int n) => await GetPokemon(n.ToString()));
+
 app.Run();
+
+public class PokemonResponse
+{
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+	public Sprites Sprites { get; set; }
+
+}
+
+public class Sprites
+{
+	[JsonPropertyName("front_default")]
+	public string FrontDefault { get; set; } = string.Empty;
+}
